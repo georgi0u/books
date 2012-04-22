86ec3b791e483ad8f536b633a3afed080e388a71
Adam Georgiou <me@adamgeorgiou.com>
2012-04-21 23:45:07 -0400

    Function added to query the db and return book descriptions

--- books/routes/index.js
+++ books/routes/index.js
@@ -1,8 +1,29 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Express' });
+};
+
+exports.getUserDescriptions = function(req, res) {    
+    var username = req.query['username'];
+    if(!username) {
+        res.writeHead(400);
+        res.end();
+        return;
+    }
+    
+    var sql_query =
+        "SELECT * FROM Descriptions as D, Users as U\
+         WHERE D.user_id = U.id\
+         AND U.username = '" + username + "'";
+
+    var cmd = db.query(sql_query);
+    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    cmd.addListener("row", function(r) {
+        var row_str = JSON.stringify(r);
+        res.write(row_str);
+    });
+        
+    cmd.addListener("end", function(e) {
+        res.end();
+    });
 };
